dec12ad6e52eac43ffac5f1c43e825cbb96abc42
Berezin Konstantin <berezinko90@gmail.com>
2020-04-11 12:09:02 +0700

    fixed remarks

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,26 @@
-const editButton = document.querySelector(".edit-button");
+//Переменные
+const editButton = document.querySelector(".profile__edit-button");
 const closeButton = document.querySelector(".popup__close-button");
 const popup = document.querySelector(".popup");
-const openPopup = function () {
-  popup.classList.add("popup_opened");
-};
-const closePopup = function () {
-  popup.classList.remove("popup_opened");
-};
-editButton.addEventListener("click", openPopup);
-closeButton.addEventListener("click", closePopup);
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
 const formElement = document.querySelector(".popup__content");
 const nameInput = document.querySelector(".popup__input_name");
 const jobInput = document.querySelector(".popup__input_description");
+//Функция открытия- закрытия ПопАпа
+const openAndClosePopup = function () {
+  popup.classList.toggle("popup_opened");
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileDescription.textContent;
+};
+//Функция отправки формы
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  nameInput.getAttribute("value");
-  jobInput.getAttribute("value");
   profileName.textContent = nameInput.value;
   profileDescription.textContent = jobInput.value;
-  closePopup();
+  openAndClosePopup();
 }
+//Слушатели
 formElement.addEventListener("submit", formSubmitHandler);
+editButton.addEventListener("click", openAndClosePopup);
+closeButton.addEventListener("click", openAndClosePopup);
