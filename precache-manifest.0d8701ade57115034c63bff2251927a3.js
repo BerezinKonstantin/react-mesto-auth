d1762103f049d3a6913a2055293f27ff2353ad2c
Berezin Konstantin <berezinko90@gmail.com>
2020-08-09 15:41:02 +0700

    Revert "fix"
    This reverts commit 06e7e4642a1680acc6c4b49be395ad39ce598b3c.

---
+++ precache-manifest.0d8701ade57115034c63bff2251927a3.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fc5e4ebb6697b46ccca901e8bad2860",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6b2fe18664c8568ef9a",
+    "url": "/static/css/main.4c81dfca.chunk.css"
+  },
+  {
+    "revision": "87fe720923abf27d7cd4",
+    "url": "/static/js/2.6e4763c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6e4763c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6b2fe18664c8568ef9a",
+    "url": "/static/js/main.97619b0e.chunk.js"
+  },
+  {
+    "revision": "c5e7b408ed1922744b94",
+    "url": "/static/js/runtime-main.e8402bce.js"
+  },
+  {
+    "revision": "c2d0901f14de58acae456bda23f80e8a",
+    "url": "/static/media/Close-Icon.c2d0901f.svg"
+  },
+  {
+    "revision": "c8252db2902c59bff2cb619de2b75c2f",
+    "url": "/static/media/Edit-Button.c8252db2.svg"
+  },
+  {
+    "revision": "34c4685038e8cf551566cb4ee96e20e5",
+    "url": "/static/media/EditAvatarIcon.34c46850.svg"
+  },
+  {
+    "revision": "14a176339fc00af3ae93be979f1593cc",
+    "url": "/static/media/Inter-Black.14a17633.woff2"
+  },
+  {
+    "revision": "b90562e0cf2dcfedca0b93ad7fa447d8",
+    "url": "/static/media/Inter-Black.b90562e0.woff"
+  },
+  {
+    "revision": "95b8a98959d1af9ab432d7ffe295ef94",
+    "url": "/static/media/Inter-Medium.95b8a989.woff"
+  },
+  {
+    "revision": "c709803c3cab6f1116039e881ecf531a",
+    "url": "/static/media/Inter-Medium.c709803c.woff2"
+  },
+  {
+    "revision": "1e081edc16d92d42aeccec760174fbf4",
+    "url": "/static/media/Inter-Regular.1e081edc.woff2"
+  },
+  {
+    "revision": "3ae6a7d3890c33d857fc00bd2e4c4820",
+    "url": "/static/media/Inter-Regular.3ae6a7d3.woff"
+  },
+  {
+    "revision": "efa6fb04512d4ea565aaa4109835b46d",
+    "url": "/static/media/Trash.efa6fb04.svg"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "/static/media/add-icon.bdfb8ebb.svg"
+  },
+  {
+    "revision": "6c74d9b53442a247e20750f5de0be939",
+    "url": "/static/media/like.6c74d9b5.svg"
+  },
+  {
+    "revision": "9fdebb6b009c16af50fae58657e30a94",
+    "url": "/static/media/like_active.9fdebb6b.svg"
+  },
+  {
+    "revision": "855a8c9865e79463f31e74f9abcb4438",
+    "url": "/static/media/logo.855a8c98.svg"
+  }
+]);
